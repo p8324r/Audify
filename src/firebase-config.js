7e4b678bc9b6ce3606a059c96e608d75976fa7b6
p8324r <112684234+p8324r@users.noreply.github.com>
2023-07-05 01:01:09 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth,GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import { initializeApp } from "firebase/app"; 
+import { getAuth,GoogleAuthProvider } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from '@firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -8,19 +8,17 @@ import { getFirestore } from '@firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfr4syx1dxVYTT5blm-l2EhKmeNeFrSFM",
-  authDomain: "audify-1094b.firebaseapp.com",
-  projectId: "audify-1094b",
-  storageBucket: "audify-1094b.appspot.com",
-  messagingSenderId: "542578621929",
-  appId: "1:542578621929:web:b4bd81185bca2a02d25f78",
-  storageBucket: "gs://audify-1094b.appspot.com"
+  /**
+   *  Refer the README . 
+   * 
+   *  Put your own Firebase service credentials here below:
+   */
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app); // Not needed
 
 export const provider = new GoogleAuthProvider();
